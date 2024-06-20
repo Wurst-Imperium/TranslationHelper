bdf763eda13a4f871b0069c8c69bbd3a722d463d
Alexander01998 <contact.wurstimperium@gmail.com>
2024-06-20 13:50:00 +0200

    Update OpenAI cost calculator

--- openai_cost.py
+++ openai_cost.py
@@ -2,61 +2,16 @@
 Utility script for calculating OpenAI API costs.
 """
 
-# OpenAI pricing per 1K tokens
+# OpenAI pricing per 1M tokens
 pricing = {
-	"gpt-3.5-turbo": {
-		"prompt": 0.0015,
-		"completion": 0.002
-	},
-	"gpt-3.5-turbo-0301": {
-		"prompt": 0.0015,
-		"completion": 0.002
-	},
-	"gpt-3.5-turbo-0613": {
-		"prompt": 0.0015,
-		"completion": 0.002
-	},
-	"gpt-3.5-turbo-1106": {
-		"prompt": 0.001,
-		"completion": 0.002
-	},
-	"gpt-3.5-turbo-16k": {
-		"prompt": 0.003,
-		"completion": 0.004
-	},
-	"gpt-3.5-turbo-16k-0613": {
-		"prompt": 0.003,
-		"completion": 0.004
-	},
-	"gpt-4": {
-		"prompt": 0.03,
-		"completion": 0.06
-	},
-	"gpt-4-0314": {
-		"prompt": 0.03,
-		"completion": 0.06
-	},
-	"gpt-4-0613": {
-		"prompt": 0.03,
-		"completion": 0.06
-	},
-	"gpt-4-1106-preview": {
-		"prompt": 0.01,
-		"completion": 0.03
-	},
-	"gpt-4-32k": {
-		"prompt": 0.06,
-		"completion": 0.12
-	},
-	"gpt-4-32k-0314": {
-		"prompt": 0.06,
-		"completion": 0.12
-	},
-	"gpt-4-32k-0613": {
-		"prompt": 0.06,
-		"completion": 0.12
-	}
+	"gpt-3.5-turbo-1106": {"prompt": 1, "completion": 2},
+	"gpt-3.5-turbo-0125": {"prompt": 0.5, "completion": 1.5},
+	"gpt-4-1106-preview": {"prompt": 10, "completion": 30},
+	"gpt-4-0125-preview": {"prompt": 10, "completion": 30},
+	"gpt-4-turbo-2024-04-09": {"prompt": 10, "completion": 30},
+	"gpt-4o-2024-05-13": {"prompt": 5, "completion": 15},
 }
+
 
 def print_usage(usages, model, estimate_other_model=True):
 	# get total usage
@@ -68,25 +23,29 @@ def print_usage(usages, model, estimate_other_model=True):
 	total_tokens = total_prompt_tokens + total_completion_tokens
 	print(f"Token usage: {total_tokens} ({total_prompt_tokens} prompt tokens, {total_completion_tokens} completion tokens)")
 
-	prompt_cost = pricing[model]["prompt"] * total_prompt_tokens / 1000
-	completion_cost = pricing[model]["completion"] * total_completion_tokens / 1000
-	total_cost = prompt_cost + completion_cost
-	print(f"Cost: ${total_cost} (${prompt_cost} for prompt tokens, ${completion_cost} for completion tokens)")
+	cost = get_estimate_string(model, total_prompt_tokens, total_completion_tokens)
+	print(f"Cost: {cost}")
 
 	if estimate_other_model:
 		if model.startswith("gpt-4"):
-			prompt_cost = pricing["gpt-3.5-turbo"]["prompt"] * total_prompt_tokens / 1000
-			completion_cost = pricing["gpt-3.5-turbo"]["completion"] * total_completion_tokens / 1000
-			total_cost = prompt_cost + completion_cost
-			print(f"Estimated GPT-3.5-Turbo cost: ${total_cost} (${prompt_cost} for prompt tokens, ${completion_cost} for completion tokens)")
+			cost_35t = get_estimate_string("gpt-3.5-turbo-0125", total_prompt_tokens, total_completion_tokens)
+			print(f"Estimated GPT-3.5-Turbo cost: {cost_35t}")
 		else:
-			prompt_cost = pricing["gpt-4"]["prompt"] * total_prompt_tokens / 1000
-			completion_cost = pricing["gpt-4"]["completion"] * total_completion_tokens / 1000
-			total_cost = prompt_cost + completion_cost
-			print(f"Estimated GPT-4 cost: ${total_cost} (${prompt_cost} for prompt tokens, ${completion_cost} for completion tokens)")
+			cost_4t = get_estimate_string("gpt-4-turbo-2024-04-09", total_prompt_tokens, total_completion_tokens)
+			print(f"Estimated GPT-4-Turbo cost: {cost_4t}")
+			cost_4o = get_estimate_string("gpt-4o-2024-05-13", total_prompt_tokens, total_completion_tokens)
+			print(f"Estimated GPT-4o cost: {cost_4o}")
+
+
+def get_estimate_string(model: str, prompt_tokens: int, completion_tokens: int):
+	prompt_cost = pricing[model]["prompt"] * prompt_tokens / 1e6
+	completion_cost = pricing[model]["completion"] * completion_tokens / 1e6
+	total_cost = prompt_cost + completion_cost
+	return f"${total_cost} (${prompt_cost} for prompt tokens, ${completion_cost} for completion tokens)"
+
 
 def estimate(model: str, prompt_tokens: int, completion_tokens: int, iterations=1):
-	prompt_cost = pricing[model]["prompt"] * prompt_tokens * iterations / 1000
-	completion_cost = pricing[model]["completion"] * completion_tokens * iterations / 1000
+	prompt_cost = pricing[model]["prompt"] * prompt_tokens * iterations / 1e6
+	completion_cost = pricing[model]["completion"] * completion_tokens * iterations / 1e6
 	total_cost = prompt_cost + completion_cost
 	return total_cost
